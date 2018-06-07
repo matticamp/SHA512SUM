d3c2cd1f4d08b986d21b0ae4256c9ca61b2112ba
matticamp <2312425+matticamp@users.noreply.github.com>
2018-06-07 13:13:10 -0400

    Update SHA512SUM.ps1

--- SHA512SUM.ps1
+++ SHA512SUM.ps1
@@ -1,3 +1,3 @@
-﻿$input=Read-Host "Enter filepath for the file to hash: "
+﻿$input=Read-Host "Enter filepath for the file to hash "
 $output=Read-Host "Enter filepath for the output file"
 Get-FileHash $input -Algorithm SHA512 | Out-File -FilePath $output
